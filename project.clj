96dc96ba1e0e5ef314ea9c81ce634eb6570e6db1
Matt Daley <m6daley@gmail.com>
2017-10-11 17:17:49 +0100

    New snapshot version; updated to use tensorflow 1.3.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clojure-tensorflow/clojure-tensorflow "0.2.4"
+(defproject clojure-tensorflow/clojure-tensorflow "0.2.5-SNAPSHOT"
   :description "A very light layer over Java interop for working with TensorFlow"
   :url "http://github.com/kieranbrowne/clojure-tensorflow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.tensorflow/tensorflow  "1.2.0"]
+                 [org.tensorflow/tensorflow  "1.3.0"]
                  [environ "1.1.0"]])
